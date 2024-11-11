b48250d1f1c7eb12055cea922cfa10cb962ddcbb
ahmadnoor99 <furqan759>
2024-11-11 12:00:37 +0500

    backend /register api done

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 require("./db/config");
+console.log("jkjkjk");
 const User = require("./db/User");
 const app = express();
 
-app.post("/register", (req, res) => {
-  res.send("API WORKS!@!!!");
+app.use(express.json());
+
+app.post("/register", async (req, res) => {
+  let user = new User(req.body);
+  let result = await user.save();
+  res.send(result);
 });
 
 app.listen(5000);
