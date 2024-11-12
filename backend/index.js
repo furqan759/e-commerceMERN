7f993edd5cfdf2722a9042f17f4a9451a2063f71
furqan759 <muhammadfurqan619@gmail.com>
2024-11-12 10:27:40 +0500

    CORS issue fixes | axios package installed

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 require("./db/config");
 console.log("jkjkjk");
 const User = require("./db/User");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.post("/register", async (req, res) => {
   let user = new User(req.body);
