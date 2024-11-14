72a14bcf2cc5e7c849f763c94ebad108c3121229
furqan759 <muhammadfurqan619@gmail.com>
2024-11-14 10:40:40 +0500

    backend: login API created

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 require("./db/config");
-console.log("jkjkjk");
 const User = require("./db/User");
 const app = express();
 
@@ -11,7 +10,23 @@ app.use(cors());
 app.post("/register", async (req, res) => {
   let user = new User(req.body);
   let result = await user.save();
+  result = result.toObject(); // To remove password in response data
+  delete result.password;
   res.send(result);
+});
+
+app.post("/login", async (req, res) => {
+  console.log(req.body);
+  if (req.body.email && req.body.password) {
+    let user = await User.findOne(req.body).select("-password"); // remove password in response data
+    if (user) {
+      res.send(user);
+    } else {
+      res.send({ result: "No User Found" });
+    }
+  } else {
+    res.send({ result: "Please Enter All Fields" });
+  }
 });
 
 app.listen(5000);
