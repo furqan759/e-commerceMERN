5c37baf690f25670a2d82d4ecb7a6ec31c3c8714
furqan759 <muhammadfurqan619@gmail.com>
2024-11-15 15:48:15 +0500

    frontend: add product API integrated

--- frontend/src/components/AddProduct.js
+++ frontend/src/components/AddProduct.js
@@ -1,8 +1,44 @@
 import { Box, Button, Grid2, TextField, Typography } from "@mui/material";
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const AddProduct = () => {
-  const auth = localStorage.getItem("users");
+  const navigate = useNavigate();
+  let auth = localStorage.getItem("user");
+  auth = JSON.parse(auth);
+  const [productDetails, setProductDetails] = useState({
+    name: "",
+    price: "",
+    category: "",
+    company: "",
+  });
+  console.log({ productDetails });
+
+  const onChangeHandler = (e) => {
+    setProductDetails({
+      ...productDetails,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const createProduct = () => {
+    axios
+      .post("http://localhost:5000/add-product", {
+        ...productDetails,
+        userId: auth._id,
+      })
+      .then(function (response) {
+        if (response?.status === 200) {
+          console.log(response);
+          navigate("/");
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
   return (
     <Box>
       <Typography textAlign={"center"} fontWeight={600} variant="h5">
@@ -10,18 +46,47 @@ export const AddProduct = () => {
       </Typography>
       <Grid2 container mt={3} mx={12} justifyContent={"center"}>
         <Grid2 item size={{ lg: 6 }} pb={2} pr={2}>
-          <TextField fullWidth label="Name" />
+          <TextField
+            fullWidth
+            label="Name"
+            name="name"
+            value={productDetails.name}
+            onChange={(e) => onChangeHandler(e)}
+          />
         </Grid2>
         <Grid2 item size={{ lg: 6 }}>
-          <TextField fullWidth label="Price" />
+          <TextField
+            fullWidth
+            label="Price"
+            name="price"
+            type="number"
+            value={productDetails.price}
+            onChange={(e) => onChangeHandler(e)}
+          />
         </Grid2>
         <Grid2 item size={{ lg: 6 }} pb={2} pr={2}>
-          <TextField fullWidth label="Category" />
+          <TextField
+            fullWidth
+            label="Category"
+            name="category"
+            value={productDetails.category}
+            onChange={(e) => onChangeHandler(e)}
+          />
         </Grid2>
         <Grid2 item size={{ lg: 6 }}>
-          <TextField fullWidth label="Company" />
+          <TextField
+            fullWidth
+            label="Company"
+            name="company"
+            value={productDetails.company}
+            onChange={(e) => onChangeHandler(e)}
+          />
         </Grid2>
-        <Button variant="outlined" sx={{ mt: 3 }}>
+        <Button
+          variant="outlined"
+          onClick={() => createProduct()}
+          sx={{ mt: 3 }}
+        >
           Create
         </Button>
       </Grid2>
